6b4df0965c2502909cebe9f58dc3442ab4f43964
hpLiu <374708234@qq.com>
2019-07-24 21:43:16 +0800

    add subgame framework

--- assets/Script/logic/view/scenes/LobbyScene.ts
+++ assets/Script/logic/view/scenes/LobbyScene.ts
@@ -1,6 +1,7 @@
 import UIManager from "../../../kernel/manager/UIManager";
 import WsSocket from "../../../kernel/net/WsSocket";
 import Globals from "../../../looker/Globals";
+import * as Consts from "../../../looker/Consts";
 
 const {ccclass, property} = cc._decorator;
 
@@ -20,7 +21,8 @@ export default class NewClass extends cc.Component {
 
     // onLoad () {}
 
-    start () {
+	start () 
+	{
         this.btn_ddz.node.on("click", function(){
 			UIManager.showDialog("prefabs/ConfirmDlg", function(){
 				var scriptCpn = this.getComponent("ConfirmDlg");
@@ -29,25 +31,48 @@ export default class NewClass extends cc.Component {
 					cc.director.loadScene("WelcomeScene")
 				}, "我是内容哦", "温馨提示");
 			})
-        }, this);
+		}, this);
+		
         this.btn_brnn.node.on("click", function(){
-			UIManager.showDialog("prefabs/ConfirmDlg", function(){
-				var scriptCpn = this.getComponent("ConfirmDlg");
-				scriptCpn.reflesh(function(menuId:number){
-					if(menuId===0) return;
-					cc.director.loadScene("WelcomeScene")
-				}, "我是内容哦", "温馨提示");
-			})
-        }, this);
+			cc.loader.loadRes("subgames/brnn/prefabs/brnn_ui", cc.Prefab, 
+				function(completeCnt:number, totalCnt:number, item:any){
+					cc.log("进度: ", completeCnt, totalCnt);
+				}, 
+				function(errorMessage, loadedResource){
+					if( errorMessage ) { cc.log( '载入预制资源失败, 原因:' + errorMessage ); return; }
+					if( !( loadedResource instanceof cc.Prefab ) ) { cc.log( '你载入的不是预制资源!' ); return; } 
+	
+					var cvs = cc.find("Canvas");
+					if( !cvs ) { cc.log("没有Canvas"); return; }
+	
+					var obj = cc.instantiate(loadedResource);
+					
+					if(obj) {
+						cvs.addChild(obj, Consts.LAYER.Panel);
+					}
+				});
+		}, this);
+		
         this.btn_fqzs.node.on("click", function(){
-			UIManager.showDialog("prefabs/ConfirmDlg", function(){
-				var scriptCpn = this.getComponent("ConfirmDlg");
-				scriptCpn.reflesh(function(menuId:number){
-					if(menuId===0) return;
-					cc.director.loadScene("WelcomeScene")
-				}, "我是内容哦", "温馨提示");
-			})
-        }, this);
+			cc.loader.loadRes("subgames/fqzs/prefabs/fqzs_ui", cc.Prefab, 
+				function(completeCnt:number, totalCnt:number, item:any){
+					cc.log("进度: ", completeCnt, totalCnt);
+				}, 
+				function(errorMessage, loadedResource){
+					if( errorMessage ) { cc.log( '载入预制资源失败, 原因:' + errorMessage ); return; }
+					if( !( loadedResource instanceof cc.Prefab ) ) { cc.log( '你载入的不是预制资源!' ); return; } 
+	
+					var cvs = cc.find("Canvas");
+					if( !cvs ) { cc.log("没有Canvas"); return; }
+	
+					var obj = cc.instantiate(loadedResource);
+					
+					if(obj) {
+						cvs.addChild(obj, Consts.LAYER.Panel);
+					}
+				});
+		}, this);
+		
         this.btn_zjh.node.on("click", function(){
 			Globals.g_ws.sendData({name: "hello", pwd: "pwd"});
 		}, this);
---
+++ assets/resources/subgames/brnn/script/brnnUI.ts
@@ -0,0 +1,18 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class BrnnUI extends cc.Component {
+
+    @property(cc.Button)
+    btn_close: cc.Button = null;
+    @property(cc.Button)
+    btn_help: cc.Button = null;
+
+    
+    start () {
+        this.btn_close.node.on("click", function(){
+			this.node.destroy();
+        }, this);
+    }
+
+}
