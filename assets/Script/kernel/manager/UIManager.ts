45ca06f3fda058910494495728e6c04594423511
hpLiu <374708234@qq.com>
2019-07-25 16:30:48 +0800

    ui管理

--- assets/Script/kernel/manager/UIManager.ts
+++ assets/Script/kernel/manager/UIManager.ts
@@ -6,8 +6,9 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class UIManager extends cc.Component {
 	private static _allUI:object = {};
+	//private static _viewStack = [];
 
-	public static initWindow(layerId:Consts.LAYER, prefabName:string, bModal:boolean, callback:Function) {
+	public static initWindow(layerId:Consts.LAYER, prefabName:string, bModal:boolean, bCloseWhenClickMask:boolean, callback:Function) {
 		if(cc.isValid(UIManager._allUI[prefabName])){
 			cc.log("allready exist: ", prefabName);
 			if(callback) { callback.apply(UIManager._allUI[prefabName]); }
@@ -28,26 +29,36 @@ export default class UIManager extends cc.Component {
 				var obj = cc.instantiate(loadedResource);
 				
 				if(obj) {
-					if(bModal){
-						CommonUtils.setModal(obj, true);
-					}
+					if(bModal){ CommonUtils.setModal(obj, bCloseWhenClickMask); }
 					cvs.addChild(obj, layerId);
 					UIManager._allUI[prefabName] = obj;
+					// if(layerId===Consts.LAYER.Panel) {
+					// 	UIManager.hidePanelsExcept(obj);
+					// }
 					if(callback) { callback.apply(obj); }
 				}
-			});
+			} 
+			);
 	}
 
 	public static showPanel(prefabName:string, callback:Function) {
-		this.initWindow(Consts.LAYER.Panel, prefabName, false, callback);
+		this.initWindow(Consts.LAYER.Panel, prefabName, true, false, callback);
 	}
 	
 	public static showPopwnd(prefabName:string, callback:Function) {
-		this.initWindow(Consts.LAYER.Popup, prefabName, true, callback);
+		this.initWindow(Consts.LAYER.Popup, prefabName, true, true, callback);
 	}
 	
 	public static showDialog(prefabName:string, callback:Function) {
-		this.initWindow(Consts.LAYER.Dialog, prefabName, true, callback);
+		this.initWindow(Consts.LAYER.Dialog, prefabName, true, false, callback);
+	}
+
+	public static hidePanelsExcept(obj) {
+		for( var k in this._allUI) {
+			if(this._allUI[k] !== obj) {
+				this._allUI[k].active = false;
+			}
+		}
 	}
 	
 		
