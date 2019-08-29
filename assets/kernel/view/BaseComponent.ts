329dbc0cf27e9dbd1047013b8d8855a45bd560eb
Visun <2361814519@qq.com>
2019-08-29 17:11:28 +0800

    ui

--- assets/kernel/view/BaseComponent.ts
+++ assets/kernel/view/BaseComponent.ts
@@ -1,6 +1,7 @@
 import EventCenter from "../event/EventCenter";
 import TimerManager from "../timer/TimerManager";
 import UIManager from "./UIManager";
+import CHandler from "../promise/CHandler";
 
 
 //---------------------------------
@@ -12,6 +13,7 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class BaseComponent extends cc.Component {
     protected m_ui:any = {};
+    protected m_dtor_listeners:any[] = [];
     
     //放回对象池时回调
     unuse(): void {
@@ -24,9 +26,20 @@ export default class BaseComponent extends cc.Component {
        
     // }
 
+    listenDestory(listener:Function, target:any){
+        this.m_dtor_listeners.push({ callback:listener, target:target });
+    }
+
     onDestroy() {
         EventCenter.instance().removeByTarget(this);
         TimerManager.instance().removeByTarget(this);
+        if(this.m_dtor_listeners){
+            for(var i=1; i<this.m_dtor_listeners.length; i++){
+                this.m_dtor_listeners[i].callback.call(this.m_dtor_listeners[i].target, this, this.node);
+            }
+            this.m_dtor_listeners.length = 0;
+            this.m_dtor_listeners = null;
+        }
         UIManager.onWindowClose(this.node);
     }
 }
