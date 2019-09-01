b05bdde769f3e9feb5df00cc915560b1bd81993b
hpLiu <374708234@qq.com>
2019-09-01 22:59:47 +0800

    Update Tree.ts

--- assets/kernel/basic/Tree.ts
+++ assets/kernel/basic/Tree.ts
@@ -1,4 +1,39 @@
+export default class TreeNode {
+	protected _id:number|string;
+	protected _pid:number|string;
+	protected _data:any;
+	protected _expanded:boolean;
+	protected _childlist:Array<TreeNode>;
 
-export default class Tree {
+	set id(v:number|string) {
+		this._id = v;
+	}
+	get id() : number|string {
+		return this._id;
+	}
 
+	set pid(v:number|string) {
+		this._pid = v;
+	}
+	get pid() : number|string {
+		return this._pid;
+	}
+
+	set data(v:any) {
+		this._data = v;
+	}
+	get data() : any {
+		return this._data;
+	}
+
+	set expanded(v:boolean) {
+		this._expanded = v;
+	}
+	get expanded() : boolean {
+		return this._expanded;
+	}
+
+	get childlist() : Array<TreeNode> {
+		return this._childlist;
+	}
 }
