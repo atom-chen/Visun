405d4266737d607f791a4f0fae653d65f964ac9a
Visun <2361814519@qq.com>
2019-09-02 18:45:11 +0800

    cache

--- assets/kernel/localcache/LocalCache.ts
+++ assets/kernel/localcache/LocalCache.ts
@@ -1,67 +1,92 @@
 //------------------------------------------
 // 本地文件缓存
 //------------------------------------------
-export default class LocalCache 
+export default class LocalCache
 {
-    private static _all_cache:{[key:string]:any} = {};
-    private _secret:any;
-    private _filename:string;
-    private _datas:object = {};
+	protected static _cacheMap:Object = {};
 
-    private constructor(filename:string) 
-    {
-        this._filename = filename;
-        this._datas = {};
-        this.load();
-    }
+	protected _name:string = null;
+	protected _localStorage:any = null;
+	protected _dataMap:Object = null;
+	
 
-    public static instance(key:string) : LocalCache
-    {
-        if(!LocalCache._all_cache[key]){ 
-            LocalCache._all_cache[key] = new LocalCache(key); 
-        }
-        return LocalCache._all_cache[key];
-    }
+	private constructor(name:string)
+	{
+		this._name = name;
+		this._localStorage = cc.sys.localStorage;
+		this._dataMap = {};
+	}
 
-    //设置密匙
-    public setSecret(mishi:string)
-    {
+	public static instance(name:string) : LocalCache
+	{	
+		let localCache:LocalCache = this._cacheMap[name] as LocalCache;
+		if (!localCache)
+		{
+			localCache = new LocalCache(name);
+			this._cacheMap[name] = localCache;
+		}
+		return localCache;
+	}
 
-    }
 
-    //读取本地文件
-    public load()
-    {
-        if( !this._filename || this._filename === "") {
-            cc.log("加载失败：没有本地文件");
-            return;
-        }
-    }
+	//获取缓存数据。如果没有该缓存且传递了defaultV则返回defaultV，否则返回空
+	public get(key:string, defaultV?:any) : any
+	{
+		let value:any = this._dataMap[key];
+		if ((value===null || value===undefined) && this._localStorage)
+		{
+			value = this._localStorage.getItem(this.localKey(key));
+			this._dataMap[key] = value;
+		}
+		if(value===null || value===undefined){
+			if(defaultV!==null && defaultV!==undefined){
+				value = defaultV;
+			}
+		}
+		cc.log("读取：", this.localKey(key), key, value);
+		return value;
+	}
 
-    //删除本地文件
-    public deleteLocalFile()
-    {
-        if( !this._filename || this._filename === "") {
-            cc.log("删除失败：没有本地文件");
-            return;
-        }
-    }
+	//更新缓存
+	public set(key:string, value:any) : void
+	{
+		if(this._dataMap[key]===value) { 
+			cc.log("无需存储：", this.localKey(key), key, value, typeof value);
+			return; 
+		}
+		this._dataMap[key] = value;
+		cc.log("存储：", this.localKey(key), key, value, typeof value);
+	    this._localStorage && this._localStorage.setItem(this.localKey(key), value);
+	}
 
-    //添加/更新
-    public update(key:string|number, value:any) : void
-    {
-        this._datas[key] = value;
-    }
+	//移除缓存一条缓存记录
+	public remove(key:string) : void
+	{
+		this.removeItem(this._dataMap, this.localKey(key));
+	}
 
-    //删除
-    public remove(key:string|number) : void
-    {
-        this._datas[key] = null;
-    }
+	//清除缓存
+	public clear() : void
+	{
+		for (var key in this._dataMap)
+		{
+			this.removeItem(this._dataMap, key);
+		}
+	}
 
-    //获取
-    public getData(key:string|number) : any
-    {
-        return this._datas[key];
-    }
+	//移除一条缓存记录
+	private removeItem(map:Object, key:string) : void
+	{
+		if (map[key]!==null &&map[key]!==undefined)
+		{
+			delete map[key];
+			this._localStorage && this._localStorage.removeItem(key);
+		}
+	}
+
+	private localKey(key:string) : string
+	{
+		return this._name + key;
+	}
+
 }
