37a4a0f1f1902ac69f8c2c584536a438a4960966
Visun <2361814519@qq.com>
2019-09-04 11:16:15 +0800

    ui

--- assets/common/script/definer/ViewDefine.ts
+++ assets/common/script/definer/ViewDefine.ts
@@ -1,17 +1,19 @@
 export default class ViewDefine {
+	public static ChipSpr = { path:"common/prefabs/ChipSpr", comp:"ChipSpr" };
+	//
+	public static LobbyUI = { path:"lobby/prefabs/UIHall", comp:"LobbyUI" };
+	public static SafeboxUI = { path:"lobby/prefabs/UISafebox", comp:"SafeboxUI" };
+	public static EmailUI = { path:"lobby/prefabs/UIEmail", comp:"EmailUI" };
+	public static ShopUI = { path:"lobby/prefabs/UIShop", comp:"ShopUI" };
+	public static KefuUI = { path:"lobby/prefabs/UIKefu", comp:"KefuUI" };
+	public static WithdrawUI = { path:"lobby/prefabs/UIWithdraw", comp:"WithdrawUI" };
+	public static SpreadUI = { path:"lobby/prefabs/UISpread", comp:"SpreadUI" };
+	public static PersonUI = { path:"lobby/prefabs/UIPerson", comp:"PersonUI" };
+	public static LoginUI = { path:"lobby/prefabs/UILogin", comp:"LoginUI" };
+	//
 	public static ddzUI = { path:"subgames/ddz/prefabs/ddz_ui", comp:"ddzUI" };
 	public static brnnUI = { path:"subgames/brnn/prefabs/brnn_ui", comp:"brnnUI" };
 	public static fqzsUI = { path:"subgames/fqzs/prefabs/fqzs_ui", comp:"fqzsUI" };
 	public static zjhUI = { path:"subgames/zjh/prefabs/zjh_ui", comp:"zjhUI" };
 	public static bjleUI = { path:"subgames/bjle/prefabs/bjle_ui", comp:"bjleUI" };
-	public static LobbyUI = { path:"lobby/prefabs/LobbyUI", comp:"LobbyUI" };
-	public static SafeboxUI = { path:"lobby/prefabs/SafeboxUI", comp:"SafeboxUI" };
-	public static EmailUI = { path:"lobby/prefabs/EmailUI", comp:"EmailUI" };
-	public static ShopUI = { path:"lobby/prefabs/ShopUI", comp:"ShopUI" };
-	public static KefuUI = { path:"lobby/prefabs/KefuUI", comp:"KefuUI" };
-	public static WithdrawUI = { path:"lobby/prefabs/WithdrawUI", comp:"WithdrawUI" };
-	public static SpreadUI = { path:"lobby/prefabs/SpreadUI", comp:"SpreadUI" };
-	public static PersonUI = { path:"lobby/prefabs/PersonUI", comp:"PersonUI" };
-	public static LoginUI = { path:"lobby/prefabs/LoginUI", comp:"LoginUI" };
-	public static ChipSpr = { path:"common/prefabs/ChipSpr", comp:"ChipSpr" };
 }
