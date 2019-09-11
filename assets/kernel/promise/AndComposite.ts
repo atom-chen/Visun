3cea7e782a60aa1d0bbd8cd858441b087e845d0c
Visun <2361814519@qq.com>
2019-09-11 18:56:32 +0800

    s

--- assets/kernel/promise/AndComposite.ts
+++ assets/kernel/promise/AndComposite.ts
@@ -2,15 +2,19 @@ import Composite from "./Composite";
 
 // 复合节点基类
 export default class AndComposite extends Composite {
-	public resolve_succ(): void {
-		throw new Error("Method not implemented.");
-	}
-	public resolve_fail(): void {
-		throw new Error("Method not implemented.");
-	}
 
 	public constructor() {
 		super();
+	}
+
+	//@overrided
+	public resolve_succ(): void {
+		throw new Error("Method not implemented.");
+	}
+
+	//@overrided
+	public resolve_fail(): void {
+		throw new Error("Method not implemented.");
 	}
 
 }
