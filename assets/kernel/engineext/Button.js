78d09dc0533ac57b8c0bf3f85bd5ed3b3a939bcd
Visun <2361814519@qq.com>
2019-09-27 14:56:34 +0800

    engine overrides

---
+++ assets/kernel/engineext/Button.js
@@ -0,0 +1,11 @@
+cc.Button.prototype._onTouchEnded = function (event) {
+    if (!this.interactable || !this.enabledInHierarchy) return;
+    console.log("===========");
+    if (this._pressed) {
+        cc.Component.EventHandler.emitEvents(this.clickEvents, event);
+        this.node.emit('click', this);
+    }
+    this._pressed = false;
+    this._updateState();
+    event.stopPropagation();
+}
