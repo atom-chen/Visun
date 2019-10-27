5bd8d3cf7590c151d2314f7276ddfda0214f77a5
hpLiu <374708234@qq.com>
2019-10-27 22:51:47 +0800

    ui

--- assets/resources/subgames/paijiu/script/PaijiuUI.ts
+++ assets/resources/subgames/paijiu/script/PaijiuUI.ts
@@ -1,8 +1,18 @@
 import BaseComponent from "../../../../kernel/view/BaseComponent";
+import CommonUtil from "../../../../kernel/utils/CommonUtil";
+import GameManager from "../../../../common/script/model/GameManager";
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class PaijiuUI extends BaseComponent {
 
+	onLoad() {
+		CommonUtil.traverseNodes(this.node, this.m_ui);
+
+		CommonUtil.addClickEvent(this.m_ui.btn_close, function(){ 
+            GameManager.getInstance().quitGame(0);
+		}, this);
+	}
+	
 }
