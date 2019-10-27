c374e7c02538848756febd965ebbd68d758137a5
hpLiu <374708234@qq.com>
2019-10-27 22:15:41 +0800

    ui

--- assets/common/script/definer/ServerConfig.ts
+++ assets/common/script/definer/ServerConfig.ts
@@ -1,21 +1,22 @@
 var server_list = {
     publish: {
-        gateServer: "http://game-cluster.sihai.com/server/gate/ip",
         mainHttpUrl: "http://sdk.sihai.com/",
         leafServer: "ws://172.18.11.133:9650"
     },
     develop: {
-        gateServer: "http://172.18.11.8:8001/server/gate/ip",
-        mainHttpUrl: "http://172.18.12.251",
+        mainHttpUrl: "http://sdk.sihai.com/",
         leafServer: "ws://172.18.11.133:9650"
     },
     jetten: {
-        gateServer: "http://172.18.11.8:8001/server/gate/ip",
-        mainHttpUrl: "http://172.18.12.251",
+        mainHttpUrl: "http://sdk.sihai.com/",
         leafServer: "ws://172.18.11.216:9650"
     },
+    mac: {
+        mainHttpUrl: "http://sdk.sihai.com/",
+        leafServer: "ws://localhost:9650"
+    }
 };
 
-var ServerConfig = server_list.develop;
+var ServerConfig = server_list.mac;
 
 export default ServerConfig;
