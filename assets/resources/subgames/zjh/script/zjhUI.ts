5d6dd15a765a3f15918bf0b9d4f378dca5db1919
hpLiu <374708234@qq.com>
2019-11-11 03:04:57 +0800

    Update zjhUI.ts

--- assets/resources/subgames/zjh/script/zjhUI.ts
+++ assets/resources/subgames/zjh/script/zjhUI.ts
@@ -1,7 +1,8 @@
 import BaseComponent from "../../../../kernel/view/BaseComponent";
 import CommonUtil from "../../../../kernel/utils/CommonUtil";
 import GameManager from "../../../../common/script/model/GameManager";
-import { zhajinhua_request } from "../../../../common/script/proto/net_zhajinhua";
+import { zhajinhua_request, zhajinhua_msgs } from "../../../../common/script/proto/net_zhajinhua";
+import EventCenter from "../../../../kernel/basic/event/EventCenter";
 
 const {ccclass, property} = cc._decorator;
 
@@ -12,6 +13,40 @@ export default class zjhUI extends BaseComponent {
         CommonUtil.traverseNodes(this.node, this.m_ui);
 
         this.initUIEvent();
+        this.initNetEvent();
+    }
+
+    onRespZhajinhuaReady(param:any) {
+
+    }
+
+    onRespZhajinhuaCompare(param:any) {
+
+    }
+
+    onRespZhajinhuaFollow(param:any) {
+
+    }
+
+    onRespZhajinhuaGiveup(param:any) {
+
+    }
+
+    onRespZhajinhuaLook(param:any) {
+
+    }
+
+    onRespZhajinhuaRaise(param:any) {
+
+    }
+
+    initNetEvent() {
+        EventCenter.getInstance().listen(zhajinhua_msgs.RespZhajinhuaReady, this.onRespZhajinhuaReady, this);
+        EventCenter.getInstance().listen(zhajinhua_msgs.RespZhajinhuaCompare, this.onRespZhajinhuaCompare, this);
+        EventCenter.getInstance().listen(zhajinhua_msgs.RespZhajinhuaFollow, this.onRespZhajinhuaFollow, this);
+        EventCenter.getInstance().listen(zhajinhua_msgs.RespZhajinhuaGiveup, this.onRespZhajinhuaGiveup, this);
+        EventCenter.getInstance().listen(zhajinhua_msgs.RespZhajinhuaLook, this.onRespZhajinhuaLook, this);
+        EventCenter.getInstance().listen(zhajinhua_msgs.RespZhajinhuaRaise, this.onRespZhajinhuaRaise, this);
     }
 
     initUIEvent() {
