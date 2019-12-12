f2fbe62de4137916f7290d10739a7d2620a32c51
hpLiu <374708234@qq.com>
2019-12-13 00:23:23 +0800

    ui

--- assets/common/script/comps/CpnCircleCD.ts
+++ assets/common/script/comps/CpnCircleCD.ts
@@ -3,12 +3,30 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class CpnCircleCD extends cc.Component {
 
-    start () {
+    _running:boolean = false;
+    _remainCD:number = 0;
+    _totalCD:number = 1;
 
+    start() {
+        this.setRemainCD(10, 10);
+        this._running = true;
+    }
+
+    setRemainCD(totalSec:number, remainSec:number) {
+        this._remainCD = remainSec;
+        this._totalCD = totalSec;
+    }
+
+    serRunning(b:boolean) {
+        this._running = b;
     }
 
     update (dt) {
-
+        if(this._running && this._remainCD > 0) {
+            this._remainCD -= dt;
+            if(this._remainCD < 0) { this._remainCD = 0; }
+            this.getComponent(cc.ProgressBar).progress = 1 - this._remainCD/this._totalCD;
+        }
     }
     
 }
