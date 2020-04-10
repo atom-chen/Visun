4c9e4fa9084d38b9689b93bd1b70cb7b5d46ea73
hpLiu <374708234@qq.com>
2020-04-10 21:59:04 +0800

    actor

---
+++ assets/resources/subgames/rpg/script/role/Actor2d.ts
@@ -0,0 +1,3 @@
+export default class Actor2d {
+	
+}
---
+++ assets/resources/subgames/rpg/script/state/State.ts
@@ -0,0 +1,36 @@
+import RoleEntity from "../role/RoleEntity";
+
+export class StateBase {
+
+	public frameUpdate() {
+
+	}
+
+	public onEnter(who: RoleEntity) {
+
+	}
+
+	public onExit(who: RoleEntity) {
+
+	}
+
+	public onComplete(who: RoleEntity) {
+
+	}
+
+}
+
+
+export class SkyMoveState extends StateBase {
+
+}
+
+
+export class GroundMoveState extends StateBase {
+
+}
+
+
+export class ActionState extends StateBase {
+
+}
