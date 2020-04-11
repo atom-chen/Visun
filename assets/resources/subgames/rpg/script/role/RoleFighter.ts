0d2ae73fc1f5cbf69d561a7163c1b1c508bfc8f1
hpLiu <374708234@qq.com>
2020-04-12 00:45:10 +0800

    Update RoleFighter.ts

--- assets/resources/subgames/rpg/script/role/RoleFighter.ts
+++ assets/resources/subgames/rpg/script/role/RoleFighter.ts
@@ -6,6 +6,7 @@ import { BEHAVIOR_STATE } from "../../../../../kernel/basic/defines/KernelDefine
 import StateMgr from "../state/StateMgr";
 import SkillMgr from "../skill/SkillMgr";
 import BuffMgr from "../buff/BuffMgr";
+import { AiCompare } from "../../../../../kernel/behaviour/AIUtil";
 
 
 export default class RoleFighter extends RoleEntity {
@@ -19,13 +20,19 @@ export default class RoleFighter extends RoleEntity {
 	protected CriAtk:number;
 	protected CriDef:number;
 	protected CriProb:number;
-	
+
 	private stateObj:StateMgr;
 	private skillObj:SkillMgr;
 	private buffObj:BuffMgr;
 	
 	public ProcCmpMyHP(cmpMode:string, hpValue:number, bPercent:boolean) : BEHAVIOR_STATE {
-		return BEHAVIOR_STATE.FAIL;
+		var finalV = this.hp;
+		if(bPercent) { finalV = this.hp/this.MaxHp; }
+		if( AiCompare(cmpMode, finalV, hpValue) ) {
+			return BEHAVIOR_STATE.SUCC;
+		} else {
+			return BEHAVIOR_STATE.FAIL;
+		}
 	}
 
 	public ProcRest(restTime:number) : BEHAVIOR_STATE {
