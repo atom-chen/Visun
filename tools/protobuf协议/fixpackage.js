4a52a1d3990b177dbb33f6f70dfaac4af239c51c
hpLiu <374708234@qq.com>
2020-05-07 16:36:41 +0800

    Update fixpackage.js

--- tools/protobuf协议/fixpackage.js
+++ tools/protobuf协议/fixpackage.js
@@ -6,32 +6,45 @@ var arguments = process.argv.splice(2);
 var pbfilename = arguments[0];
 console.log('所传递的参数是：', arguments);
 
-function getPackageName(data) {
+function fixPackageName(filepath, wantName) {
+	const data = fs.readFileSync(filepath, 'utf8').split('\n');
+
+	var pkgName = "";
+	var needWrite = false;
+
 	for(var i=0, len=data.length; i<len; i++) {
 		var str = data[i];
 		str = str.replace(';', ' ');  //去分号
 		var arr = str.split(/\s+/); 	//去空白字符
+
 		if(arr[0].match("//") || (arr[0]=="" && arr[1] && arr[1].match("//"))){
 			//是一个注释
-		}
-		else {
+		} else {
 			if( arr[0]=="package" && arr[1].indexOf("\.")<0 ) {
-				data[i] = "package "+pbfilename+";";
-				return arr[1];
+				pkgName = arr[1];
+				if(pkgName !== wantName) {
+					data[i] = "package "+wantName+";";
+					needWrite = true;
+				}
+				break;
 			}
 			else if( arr[0]=="" && arr[1]=="package" && arr[2].indexOf("\.")<0 ){
-				data[i] = "package "+pbfilename+";";
-				return arr[2];
+				pkgName = arr[2];
+				if(pkgName !== wantName) {
+					data[i] = "package "+wantName+";";
+					needWrite = true;
+				}
+				break;
 			}
 		}
 	}
-	return null;
+
+	if(needWrite) {
+		console.log("package name: ", pkgName, "  修改为：", wantName);
+		fs.writeFileSync(filepath, data.join('\n'), 'utf8');
+	} else {
+		console.log("package name: "+pkgName, "filename: "+wantName);
+	}
 }
 
-var filepath = "in/" + pbfilename + ".proto";
-const data = fs.readFileSync(filepath, 'utf8').split('\n');
-
-var mudname = getPackageName(data);
-console.log("package name: ", mudname, "  修改为：", pbfilename);
-
-fs.writeFileSync(filepath, data.join('\n'), 'utf8');
+fixPackageName("in/"+pbfilename+".proto", pbfilename);
