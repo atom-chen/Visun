02d2bc290c0cf4704ef548f583dbeda9c1c5ae25
hpLiu <374708234@qq.com>
2020-05-12 03:57:42 +0800

    ui

--- assets/resources/appenvelope/script/view/UIMain.ts
+++ assets/resources/appenvelope/script/view/UIMain.ts
@@ -11,28 +11,35 @@ export default class UIMain extends BaseComponent {
 
     start () {
         CommonUtil.traverseNodes(this.node, this.m_ui);
-
         this.initUIEvent();
+        this.switchPage(1);
+    }
+
+    switchPage(idx:number) {
+        if(idx == 1) {
+            UIManager.closeWindow(ViewDefine.UIGroup);
+            UIManager.closeWindow(ViewDefine.UIPersonal);
+            UIManager.openPanel(ViewDefine.UIChat, null);
+        } else if(idx == 2) {
+            UIManager.closeWindow(ViewDefine.UIChat);
+            UIManager.closeWindow(ViewDefine.UIPersonal);
+            UIManager.openPanel(ViewDefine.UIGroup, null);
+        } else {
+            UIManager.closeWindow(ViewDefine.UIGroup);
+            UIManager.closeWindow(ViewDefine.UIChat);
+            UIManager.openPanel(ViewDefine.UIPersonal, null);
+        }
     }
 
     initUIEvent() {
         CommonUtil.addClickEvent(this.m_ui.btn_chat, function(){ 
-            UIManager.closeWindow(ViewDefine.UIGroup);
-            UIManager.closeWindow(ViewDefine.UIPersonal);
-            UIManager.openPanel(ViewDefine.UIChat, null);
+            this.switchPage(1);
         }, this);
         CommonUtil.addClickEvent(this.m_ui.btn_group, function(){ 
-            UIManager.closeWindow(ViewDefine.UIChat);
-            UIManager.closeWindow(ViewDefine.UIPersonal);
-            UIManager.openPanel(ViewDefine.UIGroup, null);
+            this.switchPage(2);
         }, this);
         CommonUtil.addClickEvent(this.m_ui.btn_person, function(){ 
-            if(!LoginMgr.getInstance().checkLogin(true)){
-                return;
-            }
-            UIManager.closeWindow(ViewDefine.UIGroup);
-            UIManager.closeWindow(ViewDefine.UIChat);
-            UIManager.openPanel(ViewDefine.UIPersonal, null);
+            this.switchPage(3);
 		}, this);
     }
 
