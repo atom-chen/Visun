356e5e81cb97ccc8c09b4381a73147bbd8415a85
hpLiu <374708234@qq.com>
2020-05-13 22:16:06 +0800

    Update UIEnvelopeSend.ts

--- assets/resources/appenvelope/script/view/UIEnvelopeSend.ts
+++ assets/resources/appenvelope/script/view/UIEnvelopeSend.ts
@@ -2,9 +2,9 @@ import BaseComponent from "../../../../kernel/view/BaseComponent";
 import CommonUtil from "../../../../kernel/utils/CommonUtil";
 import { isEmpty } from "../../../../kernel/utils/GlobalFuncs";
 import UIManager from "../../../../kernel/view/UIManager";
-import { luck_request } from "../../../../common/script/proto/net_luck";
-import { chat_request } from '../../../../common/script/proto/net_chat';
+import { chat_request, chat_msgs } from '../../../../common/script/proto/net_chat';
 import ChatMgr from '../../../../common/script/model/ChatMgr';
+import EventCenter from "../../../../kernel/basic/event/EventCenter";
 
 const {ccclass, property} = cc._decorator;
 
@@ -15,16 +15,22 @@ export default class UIEnvelopeSend extends BaseComponent {
         CommonUtil.traverseNodes(this.node, this.m_ui);
 
         this.initUIEvent();
+
+        EventCenter.getInstance().listen(chat_msgs.GroupSendRewardResp, function(param){
+            CommonUtil.safeDelete(this);
+        }, this);
     }
 
     initUIEvent() {
         CommonUtil.addClickEvent(this.m_ui.btn_ok, function() {
-            var money = parseInt(this.m_ui.edit_money.getComponent(cc.EditBox).string);
-            var count = parseInt(this.m_ui.edit_count.getComponent(cc.EditBox).string);
+            var money = this.m_ui.edit_money.getComponent(cc.EditBox).string;
+            var count = this.m_ui.edit_count.getComponent(cc.EditBox).string;
 			if(isEmpty(money) || isEmpty(count)) {
 				UIManager.toast("请输入有效的红包信息");
 				return;
             }
+            money = parseInt(money);
+            count = parseInt(count);
             cc.log("发红包", money, count);
             chat_request.GroupSendReward({
                 GroupId: ChatMgr.getInstance().getChatGroup(),
@@ -34,7 +40,8 @@ export default class UIEnvelopeSend extends BaseComponent {
                     Money: money,
                 }
             })
-		}, this);
+        }, this);
+        
 		CommonUtil.addClickEvent(this.m_ui.btn_no, function(){
             CommonUtil.safeDelete(this);
 		}, this);
