0619069d3a61ea2a8255e2d85e42b0f9da69df0a
hpLiu <374708234@qq.com>
2020-08-27 18:24:27 +0800

    ui

--- assets/common/script/model/SimplePlayer.ts
+++ assets/common/script/model/SimplePlayer.ts
@@ -1,6 +1,6 @@
 export default class SimplePlayer {
-	UserID:number = 1;
-    Sex:number = 2; 		//性别
-    Gold:number = 3; 		//金币
-	HeadID:number = 4; 		//头像标识
+	UserID:number = 0;
+    Sex:number = 0; 		//性别
+    Gold:number = 0; 		//金币
+	HeadID:number = 0; 		//头像标识
 }
