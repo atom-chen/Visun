8802019a280417bac281f9ad17abc326f9836fde
hpLiu <374708234@qq.com>
2020-08-31 08:00:46 +0800

    Update UIbjle.ts

--- assets/resources/subgames/bjle/script/view/UIbjle.ts
+++ assets/resources/subgames/bjle/script/view/UIbjle.ts
@@ -170,10 +170,9 @@ export default class UIbjle extends BaseComponent {
 		AudioManager.getInstance().playEffectAsync("appqp/audios/chipmove", false);
     }
 
-	//准备阶段
 	private BaccaratStateOpenResp(param) {
 		this.isJoined = false;
-		this.m_ui.CpnGameState.getComponent(CpnGameState).setZhunbei();
+		this.m_ui.CpnGameState.getComponent(CpnGameState).setKaipai();
 		TimerManager.delTimer(this.tmrState);
 		this.tmrState = TimerManager.loopSecond(1, 3, new CHandler(this, this.onStateTimer), true);
 		this.m_ui.cardLayer.active = false;
@@ -183,7 +182,7 @@ export default class UIbjle extends BaseComponent {
 
 	//开局：洗牌发牌
 	private BaccaratStateStart(param) {
-		this.m_ui.CpnGameState.getComponent(CpnGameState).setFapai();
+		this.m_ui.CpnGameState.getComponent(CpnGameState).setZhunbei();
 		TimerManager.delTimer(this.tmrState);
 		this.tmrState = TimerManager.loopSecond(1, 3, new CHandler(this, this.onStateTimer), true);
 		this.m_ui.cardLayer.active = false;
