868883944429bde440d23f74e8b4d66a69e6ab2c
hpLiu <374708234@qq.com>
2020-09-11 04:24:55 +0800

    nn

--- assets/resources/subgames/tbnn/scripts/TbnnMgr.ts
+++ assets/resources/subgames/tbnn/scripts/TbnnMgr.ts
@@ -1,6 +1,9 @@
 import ModelBase from "../../../../kernel/model/ModelBase";
 import { tbcowcow } from "../../../../../declares/tbcowcow";
 import GameManager from "../../../../common/script/model/GameManager";
+import { isNil } from "../../../../kernel/utils/GlobalFuncs";
+import { gamecomm } from "../../../../../declares/gamecomm";
+import CommonUtil from "../../../../kernel/utils/CommonUtil";
 
 export default class TbnnMgr extends ModelBase {
 	private static _instance:TbnnMgr = null;
@@ -16,11 +19,48 @@ export default class TbnnMgr extends ModelBase {
 	}
 	
 	on_clear(): void {
-		
+		this._players = {};
 	}
+
+	private _players : {[key:number]:tbcowcow.ITbcowcowPlayer} = {};
 
 	getEnterData() : tbcowcow.ITbcowcowSceneResp {
         return GameManager.getInstance().getEnterData();
     }
+	
+	clearPlayers() {
+        this._players = {};
+    }
+
+    addPlayer(man:tbcowcow.ITbcowcowPlayer) {
+        if(isNil(this._players[(man.MyInfo as gamecomm.IPlayerInfo).UserID])) {
+            this._players[(man.MyInfo as gamecomm.IPlayerInfo).UserID] = man;
+        } else {
+            CommonUtil.simpleCopy(this._players[(man.MyInfo as gamecomm.IPlayerInfo).UserID], man);
+        }
+    }
+
+    removePlayer(uid:number) {
+        this._players[uid] = null;
+        delete this._players[uid];
+	}
+
+	getPlayer(uid:number) : tbcowcow.ITbcowcowPlayer {
+		return this._players[uid];
+    }
+
+    getPlayerList() : {[key:number]:tbcowcow.ITbcowcowPlayer} {
+        return this._players;
+    }
+
+    fighterCnt() : number {
+        var cnt = 0;
+        for(var uid in this._players) {
+            if(!isNil(this._players[uid])) {
+                cnt++;
+            }
+        }
+        return cnt;
+	}
 	
 }
--- assets/resources/subgames/tbnn/scripts/UItbnn.ts
+++ assets/resources/subgames/tbnn/scripts/UItbnn.ts
@@ -14,6 +14,9 @@ import CpnGameState from "../../../appqp/script/comps/CpnGameState";
 import { GameKindEnum } from "../../../../common/script/definer/ConstDefine";
 import ViewDefine from "../../../../common/script/definer/ViewDefine";
 import UIManager from "../../../../kernel/view/UIManager";
+import { isNil } from "../../../../kernel/utils/GlobalFuncs";
+import LoginUser from "../../../../common/script/model/LoginUser";
+import { gamecomm } from "../../../../../declares/gamecomm";
 
 
 const MAX_SOLDIER = 5;
@@ -50,20 +53,88 @@ export default class UItbnn extends BaseComponent {
         ProcessorMgr.getInstance().getProcessor(ChannelDefine.game).setPaused(false);
     }
 
+    //玩家的UI位置
+    private playerIndex(player:tbcowcow.ITbcowcowPlayer) : number {
+		if(isNil(player)){ return -1; }
+        var hero = TbnnMgr.getInstance().getPlayer(LoginUser.getInstance().UserId);
+        if(isNil((player.MyInfo as gamecomm.IPlayerInfo).ChairID)) { return -1; }
+        if(isNil(hero)) { return (player.MyInfo as gamecomm.IPlayerInfo).ChairID; }
+		if((hero.MyInfo as gamecomm.IPlayerInfo).ChairID===0) { return (player.MyInfo as gamecomm.IPlayerInfo).ChairID; }
+		return ((player.MyInfo as gamecomm.IPlayerInfo).ChairID-(hero.MyInfo as gamecomm.IPlayerInfo).ChairID+MAX_SOLDIER) % MAX_SOLDIER;
+    }
+
+    private playerIdx(uid:number) : number {
+        var p = TbnnMgr.getInstance().getPlayer(uid);
+        return this.playerIndex(p);
+    }
+
     private playFapaiAni() {
         var nn = 0;
         for(var i=0; i<MAX_SOLDIER; i++){
-            this._handors[i].resetCards([0,0,0,0,0]);
-            var fromPos = CommonUtil.convertSpaceAR(this.m_ui.cpzhuang, this._handors[i].node);
-            for(var j=0; j<CARD_CNT; j++) {
-                nn++;
-                CommonUtil.bezierTo3(this._handors[i].node.children[j], fromPos, this._handors[i].getComponent(CpnHandcard2).getPosByIndex(j), 0.4, nn*0.06);
+            if(this.getPlayerByIndex(i)) {
+                this._handors[i].resetCards([0,0,0,0,0]);
+                var fromPos = CommonUtil.convertSpaceAR(this.m_ui.cpzhuang, this._handors[i].node);
+                for(var j=0; j<CARD_CNT; j++) {
+                    nn++;
+                    CommonUtil.bezierTo3(this._handors[i].node.children[j], fromPos, this._handors[i].getComponent(CpnHandcard2).getPosByIndex(j), 0.4, nn*0.06);
+                }
             }
         }
     }
 
-    private TbcowcowSceneResp(data:tbcowcow.ITbcowcowSceneResp) {
+    getPlayerByIndex(idx:number) : tbcowcow.ITbcowcowPlayer {
+        if(idx < 0) { return null; }
+        var mans = TbnnMgr.getInstance().getPlayerList();
+        if(mans) {
+            for(var uid in mans) {
+                if(this.playerIndex(mans[uid]) == idx) {
+                    return mans[uid];
+                }
+            }
+        }
+        return null;
+    }
 
+    private refreshFighter(uid) {
+        var man = TbnnMgr.getInstance().getPlayer(uid);
+        var idx = this.playerIdx(uid);
+        if(idx >= 0) {
+            this._pnodes[idx].active = true;
+            this._playerCpns[idx].setName((man.MyInfo as gamecomm.IPlayerInfo).Name);
+            this._playerCpns[idx].setMoneyStr(CommonUtil.formRealMoney((man.MyInfo as gamecomm.IPlayerInfo).Gold));
+            this._playerCpns[idx].setHeadImg((man.MyInfo as gamecomm.IPlayerInfo).FaceID);
+            this._handors[idx].resetCards(man.Cards.Cards);
+            this._handors[idx].playOpen(false);
+        }
+    }
+
+    refreshPlayerByIndex(idx:number, flag:boolean = true) {
+        if(idx < 0) { return; }
+        var man = this.getPlayerByIndex(idx);
+        if(isNil(man)) {
+            this._pnodes[idx].active = false;
+            return;
+        }
+        if(flag) {
+            this.refreshFighter((man.MyInfo as gamecomm.IPlayerInfo).UserID);
+        }
+    }
+
+    private TbcowcowSceneResp(param:tbcowcow.ITbcowcowSceneResp) {
+        if(isNil(param)) { return; }
+
+        TbnnMgr.delInstance();
+        TbnnMgr.getInstance();
+        if(param.AllPlayers) {
+            for(var ii in param.AllPlayers) {
+                TbnnMgr.getInstance().addPlayer(param.AllPlayers[ii]);
+            }
+        }
+
+        this.m_ui.labgameuuid.getComponent(cc.Label).string = "牌局号：" + param.Inning;
+        for(var n=0; n<MAX_SOLDIER; n++) {
+            this.refreshPlayerByIndex(n);
+        }
     }
 
     private TbcowcowStateFreeResp(data:tbcowcow.ITbcowcowSceneResp) {
