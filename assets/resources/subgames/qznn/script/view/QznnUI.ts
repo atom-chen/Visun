ee7c1a13c8f1eeb67992f4532681975921a36bf1
hpLiu <374708234@qq.com>
2020-09-10 11:00:01 +0800

    mm

--- assets/resources/subgames/qznn/script/view/QznnUI.ts
+++ assets/resources/subgames/qznn/script/view/QznnUI.ts
@@ -10,26 +10,54 @@ import QznnMgr from "../model/QznnMgr";
 import ProcessorMgr from "../../../../../kernel/net/processor/ProcessorMgr";
 import ChannelDefine from "../../../../../common/script/definer/ChannelDefine";
 import EventCenter from "../../../../../kernel/basic/event/EventCenter";
+import CpnPlayer1 from "../../../../appqp/script/comps/CpnPlayer1";
+import CpnHandcard2 from "../../../../appqp/script/comps/CpnHandcard2";
 
 
-var MAX_PLAYER = 5;
+const MAX_SOLDIER = 5;
+const CARD_CNT = 5;
+
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class QznnUI extends BaseComponent {
-
-	_pnList:Array<{[key:string]:cc.Node}> = [];
+	private _pnodes:Array<cc.Node> = [];
+    private _playerCpns:Array<CpnPlayer1> = [];
+    private _handors:Array<CpnHandcard2> = [];
+    private _cdCpns:Array<CpnCircleCD> = [];
 
 	onLoad() {
 		CommonUtil.traverseNodes(this.node, this.m_ui);
+
+		for(var i = 0; i <= 4; i++) {
+            var nd = this.m_ui["p"+i];
+            this._pnodes.push(nd);
+            this._playerCpns.push(nd.getChildByName("CpnPlayer1").getComponent(CpnPlayer1));
+            this._handors.push(nd.getChildByName("CpnHandcard2").getComponent(CpnHandcard2));
+            this._cdCpns.push(nd.getChildByName("CpnCircleCD").getComponent(CpnCircleCD));
+        }
 
 		this.initUIEvent();
 		this.initNetEvent();
 
 		this.QzcowcowSceneResp(QznnMgr.getInstance().getEnterData());
-        ProcessorMgr.getInstance().getProcessor(ChannelDefine.game).setPaused(false);
+		ProcessorMgr.getInstance().getProcessor(ChannelDefine.game).setPaused(false);
+		
+		this.playFapaiAni();
 	}
+
+	private playFapaiAni() {
+        var nn = 0;
+        for(var i=0; i<MAX_SOLDIER; i++){
+            this._handors[i].resetCards([0,0,0,0,0]);
+            var fromPos = CommonUtil.convertSpaceAR(this.m_ui.cpzhuang, this._handors[i].node);
+            for(var j=0; j<CARD_CNT; j++) {
+                nn++;
+                CommonUtil.bezierTo3(this._handors[i].node.children[j], fromPos, this._handors[i].getComponent(CpnHandcard2).getPosByIndex(j), 0.4, nn*0.06);
+            }
+        }
+    }
 
 	private QzcowcowSceneResp(data:qzcowcow.IQzcowcowSceneResp) {
 
--- assets/resources/subgames/tbnn/scripts/UItbnn.ts
+++ assets/resources/subgames/tbnn/scripts/UItbnn.ts
@@ -7,21 +7,54 @@ import { tbcowcow } from "../../../../../declares/tbcowcow";
 import TbnnMgr from "./TbnnMgr";
 import ProcessorMgr from "../../../../kernel/net/processor/ProcessorMgr";
 import ChannelDefine from "../../../../common/script/definer/ChannelDefine";
+import CpnPlayer1 from "../../../appqp/script/comps/CpnPlayer1";
+import CpnHandcard2 from "../../../appqp/script/comps/CpnHandcard2";
+import CpnCircleCD from "../../../appqp/script/comps/CpnCircleCD";
+
+
+const MAX_SOLDIER = 5;
+const CARD_CNT = 5;
 
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class UItbnn extends BaseComponent {
+    private _pnodes:Array<cc.Node> = [];
+    private _playerCpns:Array<CpnPlayer1> = [];
+    private _handors:Array<CpnHandcard2> = [];
+    private _cdCpns:Array<CpnCircleCD> = [];
 
     start () {
         CommonUtil.traverseNodes(this.node, this.m_ui);
+
+        for(var i = 0; i <= 4; i++) {
+            var nd = this.m_ui["p"+i];
+            this._pnodes.push(nd);
+            this._playerCpns.push(nd.getChildByName("CpnPlayer1").getComponent(CpnPlayer1));
+            this._handors.push(nd.getChildByName("CpnHandcard2").getComponent(CpnHandcard2));
+            this._cdCpns.push(nd.getChildByName("CpnCircleCD").getComponent(CpnCircleCD));
+        }
 
 		this.initUIEvent();
         this.initNetEvent();
         
         this.TbcowcowSceneResp(TbnnMgr.getInstance().getEnterData());
         ProcessorMgr.getInstance().getProcessor(ChannelDefine.game).setPaused(false);
+
+        this.playFapaiAni();
+    }
+
+    private playFapaiAni() {
+        var nn = 0;
+        for(var i=0; i<MAX_SOLDIER; i++){
+            this._handors[i].resetCards([0,0,0,0,0]);
+            var fromPos = CommonUtil.convertSpaceAR(this.m_ui.cpzhuang, this._handors[i].node);
+            for(var j=0; j<CARD_CNT; j++) {
+                nn++;
+                CommonUtil.bezierTo3(this._handors[i].node.children[j], fromPos, this._handors[i].getComponent(CpnHandcard2).getPosByIndex(j), 0.4, nn*0.06);
+            }
+        }
     }
 
     private TbcowcowSceneResp(data:tbcowcow.ITbcowcowSceneResp) {
