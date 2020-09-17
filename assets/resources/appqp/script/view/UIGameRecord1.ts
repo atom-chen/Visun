2a1cc37066e083e59b8c8135724f4f13b1af1356
hpLiu <374708234@qq.com>
2020-09-17 19:41:05 +0800

    0

--- assets/resources/appqp/script/view/UIGameRecord1.ts
+++ assets/resources/appqp/script/view/UIGameRecord1.ts
@@ -1,4 +1,5 @@
 import CommonUtil from "../../../../kernel/utils/CommonUtil";
+import { isNil } from "../../../../kernel/utils/GlobalFuncs";
 import BaseComponent from "../../../../kernel/view/BaseComponent";
 
 const {ccclass, property} = cc._decorator;
@@ -6,6 +7,10 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class UIGameRecord1 extends BaseComponent {
 
+    @property(cc.Prefab)
+    listItem: cc.Prefab = null;
+
+    private _gameType = 0;
     private winColor = cc.color(7,123,38,255);
     private loseColor = cc.color(218,32,32,255);
 
@@ -17,30 +22,38 @@ export default class UIGameRecord1 extends BaseComponent {
         }, this);
     }
 
-    private setViewData(info) {
-        info = [];
+    private setViewData(gameType:number) {
+        this._gameType = gameType;
+
+        var info = [];
         for(var n=0; n<20; n++) {
             var data = {
-                No: n,
                 Code: "wsfdwp35293egiyzaqp432d2",
                 Room: "高级房",
                 Money: (CommonUtil.getRandomInt(1,100)-50.5) * 100,
-                EndTime: (new Date()).valueOf()
+                EndTime: (new Date()).valueOf() / 1000 + n
             }
             info.push(data);
         }
+        this.refleshList(info);
+    }
 
-        for(var i in info) {
-            var item = cc.instantiate(this.m_ui.listItem);
+    private refleshList(info:Array<any>) {
+        this.m_ui.content.removeAllChildren(true);
+        var len = info.length;
+        var Order = 0;
+        for(var i=len-1; i>=0; i--) {
+            var item = item = cc.instantiate(this.listItem);
             this.m_ui.content.addChild(item);
-            item.getChildByName("lab_no").getComponent(cc.Label).string = info[i].No;
+            Order++;
+            item.getChildByName("lab_no").getComponent(cc.Label).string = ""+Order;
             item.getChildByName("lab_code").getComponent(cc.Label).string = info[i].Code;
             item.getChildByName("lab_room").getComponent(cc.Label).string = info[i].Room;
             item.getChildByName("lab_win").getComponent(cc.Label).string = info[i].Money;
+            item.getChildByName("lab_time").getComponent(cc.Label).string = CommonUtil.formatTime(info[i].EndTime);
             if(info[i].Money >= 0) {
                 item.getChildByName("lab_win").color = this.winColor;
             }
-            item.getChildByName("lab_time").getComponent(cc.Label).string = CommonUtil.formatTime(info[i].EndTime/1000);
         }
     }
 
