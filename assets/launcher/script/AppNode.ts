974a30d8a265cca2ccea6f087992d1b5322d7302
hpLiu <374708234@qq.com>
2020-09-19 23:45:12 +0800

    remove unused files

--- assets/launcher/script/AppNode.ts
+++ assets/launcher/script/AppNode.ts
@@ -4,20 +4,25 @@
 import KernelEvent from "../../kernel/basic/defines/KernelEvent";
 import EventCenter from "../../kernel/basic/event/EventCenter";
 import TimerManager from "../../kernel/basic/timer/TimerManager";
+import CommonUtil from "../../kernel/utils/CommonUtil";
 import { newHandler } from "../../kernel/utils/GlobalFuncs";
 import InitLogic from "./InitLogic";
-import UILoading from "./UILoading";
 
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class AppNode extends cc.Component {
+    private m_ui: {[key:string]:cc.Node} = {};
+    private labProg: cc.Label = null;
     private _tmr: number = 0;
 
     onLoad () {
         cc.game.addPersistRootNode(this.node);
         InitLogic.run(this);
+
+        CommonUtil.traverseNodes(this.node, this.m_ui);
+        this.labProg = this.m_ui.labelProg.getComponent(cc.Label);
 
         this.onUiLoadingBegin();
 
@@ -28,17 +33,18 @@ export default class AppNode extends cc.Component {
 
     onUiLoadingBegin() {
         this._tmr = TimerManager.delTimer(this._tmr);
-        this.node.getChildByName("UILoading").active = true;
+        this.m_ui.UILoading.active = true;
+        this.m_ui.sprLoading.runAction(cc.repeatForever(cc.rotateBy(1, 360)));
     }
 
     onUiLoadingProgress(sub:number, total:number) {
         this._tmr = TimerManager.delTimer(this._tmr);
-        this.node.getChildByName("UILoading").getComponent(UILoading).onUiLoadingProgress(sub, total);
+        this.labProg.string = sub + "/" + total;
     }
 
     onUiLoadingFinish() {
         this._tmr = TimerManager.delayFrame(3, newHandler(function(tmr){
-            this.node.getChildByName("UILoading").active = false;
+            this.m_ui.UILoading.active = false;
         }, this));
     }
 
