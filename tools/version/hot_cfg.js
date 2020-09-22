689f7252a3e209b3c34890b34a1aa0adc844823c
hpLiu <374708234@qq.com>
2020-09-22 09:09:40 +0800

    0

--- tools/version/hot_cfg.js
+++ tools/version/hot_cfg.js
@@ -1,5 +1,5 @@
 module.exports = {
-	version : "1.13",
+	version : "1.14",
 	packageUrl : "http://192.168.1.101:8081/hotupdate/fingerqp/",
 	srcDir : "../../build/jsb-link",
 	dstDir : "/users/liuhaopeng/documents/nodeserver/hotupdate/fingerqp"
