fc0e4b55cb4ccd95847efe4f5dad33fc7f0943f8
hpLiu <374708234@qq.com>
2020-09-26 21:04:56 +0800

    kefu

--- assets/resources/lobby/script/view/kefu/UIKefu.ts
+++ assets/resources/lobby/script/view/kefu/UIKefu.ts
@@ -5,8 +5,16 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class UIKefu extends BaseComponent {
+
 	onLoad() {
 		CommonUtil.traverseNodes(this.node, this.m_ui);
 		CommonUtil.addClickEvent(this.m_ui.btn_close, function(){ CommonUtil.safeDelete(this); }, this);
+		CommonUtil.addClickEvent(this.m_ui.btn_ok1, this.onBtnOK, this);
 	}
+
+	onBtnOK() {
+		var cont = this.m_ui.editCont.getComponent(cc.EditBox).string;
+		cc.log(cont);
+	}
+
 }
