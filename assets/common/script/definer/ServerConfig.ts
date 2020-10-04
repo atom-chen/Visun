c236b165c39297d0e30ab42544136f85417e4ccf
hpLiu <374708234@qq.com>
2020-10-04 16:56:42 +0800

    热更url配置

--- assets/common/script/definer/ServerConfig.ts
+++ assets/common/script/definer/ServerConfig.ts
@@ -1,26 +1,34 @@
 //服务器列表
 // mainHttpUrl: http域名
 // leafServer:  游戏连接ip地址/域名
+var hotdominList = [
+    "http://192.168.1.101:8081/hotupdate/fingerqp/",
+];
+
 var server_list = {
     publish: {
         mainHttpUrl: "",
         leafServer: "ws://192.168.0.108:9950",
-        chatServer: ""
+        chatServer: "",
+        hotdomins: hotdominList,
     },
     develop: {
         mainHttpUrl: "",
         leafServer: "ws://8.129.175.179:9950",
-        chatServer: ""
+        chatServer: "",
+        hotdomins: hotdominList,
     },
     jetten: {
         mainHttpUrl: "",
         leafServer: "ws://127.0.0.1:9950",
-        chatServer: ""
+        chatServer: "",
+        hotdomins: hotdominList,
     },
     mac: {
         mainHttpUrl: "",
         leafServer: "ws://192.168.1.100:9950",
-        chatServer: ""
+        chatServer: "",
+        hotdomins: hotdominList,
     }
 };
 
